21a4ec43291106571c214400dcbbaceb4c94eb19
RatLad69 <quirinus613@gmail.com>
2024-01-07 02:50:55 -0600

    added update

--- dictionary.go
+++ dictionary.go
@@ -1,7 +1,49 @@
 package main
 
 type Dictionary map[string]string
+type DictionaryErr string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+const ErrUnknown = DictionaryErr("unknown word. cannot define")
+const ErrWordExists = DictionaryErr("word already defined. cannot add")
+const ErrWordNotFound = DictionaryErr("word is not defined. cannot update")
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrUnknown
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrUnknown:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrUnknown:
+		return ErrWordNotFound
+	case nil:
+		d[word] = newDefinition
+		return nil
+	default:
+		return err
+	}
 }
